0b703c7f2b842c135d025005466e1955ffd66080
Noah Herron <61994195+njherron@users.noreply.github.com>
2024-12-12 21:31:58 -0500

    items tab (#3)
    * added some models for items and recipes. Worked on fetching and parsing item data from ao-bin-dumps
    
    * started with ui
    
    * Merge og origin into this fork
    connected item lookup to type ahead
    connected list to item price lookup
    
    * Nested tabs
    add item on click and enter press
    
    * fixed item names in the table
    updated readme with some note about features
    added item type enum
    added type property to Item
    added print_recipe method to recipe
    
    * created ItemRef and converted Recipe to use it
    added placeholders for tier and item type dropdowns on market price tab
    Added completer to crafting item input
    populate table with recipe information
    added items tab
    initial work on displaying item information for debug
    
    * fix recipe gathering for both lists and dicts
    fixed name setting issue from ItemRef refactor
    added data to items list items for future filtering
    basic filtering looks for name values
    starting adding resource types harvested form items.json
    
    * added external item model for data ingestion from bin dumps
    refactored domain item creation logic
    added resource files for raw and formatted items
    currently displaying raw items in item tab
    fixed json to string method to prevent infinite recursion
    added tier enum

--- data/constants/recipes/recipe.py
+++ data/constants/recipes/recipe.py
@@ -11,25 +11,38 @@ class ItemType(Enum):
     WEAPON = auto()
     GATHER = auto()
     TOOL = auto()
+    TOKEN = auto()
+    ARENA_TOKEN = auto()
 
 
-class Item:
+class Tier(Enum):
+    UNKNOWN = auto()
+    ONE = auto()
+    TWO = auto()
+    THREE = auto()
+    FOUR = auto()
+    FIVE = auto()
+    SIX = auto()
+    SEVEN = auto()
+    EIGHT = auto()
 
-    def __init__(self, item_id: str, recipe: Optional['Recipe'] = None):
-        self.name = item_id
-        self.description = ''
-        self.recipe = recipe
-        # item ID used with AO Data
+
+class ItemRef:
+    def __init__(self, item_id: str, item_name: str = None):
         self.item_id = item_id
-        self.type = ItemType.UNKNOWN
+        self.item_name = item_name
+
+
+class Item(ItemRef):
+
+    def __init__(self, item_id: str, recipes: Optional['[Recipe]'] = None):
+        ItemRef.__init__(self, item_id, item_id)
+        self.description = ''
+        self.recipes = recipes if recipes is not None else []
+        self.item_type = ItemType.UNKNOWN
 
     def __str__(self):
-        if self.recipe and self.recipe.inputs:
-            to_string = f"To craft {self.item_id}, you need:\n"
-            for i in self.recipe.inputs:
-                to_string += f"\t{i.quantity}: {i.content.item_id}\n"
-                return to_string
-        return f"Item: {self.item_id}, No recipe available"
+        return to_json_string(self, indent=2)
 
     def __repr__(self):
         return self.__str__()
@@ -45,6 +58,40 @@ class Item:
 
 class Recipe:
 
-    def __init__(self, inputs: List[Quantity['Item']] = None, output: Quantity['Item'] = None):
+    def __init__(self, inputs: List[Quantity[ItemRef]] = None, output: Quantity[ItemRef] = None):
         self.inputs = inputs if inputs is not None else []
         self.output = output
+
+    def __str__(self):
+        return to_json_string(self, indent=2)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def to_json_string(obj, indent=0, visited=None):
+    if visited is None:
+        visited = set()
+
+    def format_value(value, indent_level):
+        if isinstance(value, (str, int, float, bool, type(None))):
+            return f"\"{value}\"" if isinstance(value, str) else str(value)
+        elif isinstance(value, list):
+            items = [format_value(item, indent_level + 2) for item in value]
+            return "[\n" + ",\n".join(
+                " " * (indent_level + 2) + item for item in items) + "\n" + " " * indent_level + "]"
+        elif isinstance(value, Enum):
+            return f"\"{value.name}\""
+        elif hasattr(value, '__dict__'):
+            return to_json_string(value, indent_level, visited)
+        else:
+            return str(value)
+
+    if id(obj) in visited:
+        return "\"<circular reference>\""
+
+    visited.add(id(obj))
+    obj_dict = obj.__dict__
+    items = [f"\"{key}\": {format_value(value, indent + 2)}" for key, value in obj_dict.items()]
+    visited.remove(id(obj))
+    return "{\n" + ",\n".join(" " * (indent + 2) + item for item in items) + "\n" + " " * indent + "}"
